4fc220ff51dfe659aa03905fc7e2e04137769807
Dani <daniell0626@naver.com>
2026-08-13 01:09:58 +0000

    그냥 분리시켜

--- backend/hosting.js
+++ backend/hosting.js
@@ -1,8 +1,18 @@
 import { serveDir } from "jsr:@std/http/file-server";
 
-Deno.serve((req) => {
+// 서버 시작
+Deno.serve({ port: 8000 }, (req) => {
   return serveDir(req, {
     fsRoot: "../frontend",
     urlRoot: "",
   });
 });
+
+// Windows 기본 브라우저 열기
+new Deno.Command("powershell", {
+  args: [
+    "-NoProfile",
+    "-Command",
+    "Start-Process 'http://localhost:8000'",
+  ],
+}).output();
